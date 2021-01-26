7b19978b8b6588fd8ac855fdaf269aa772f1ef3c
erythrocyte <marseille.tatar@yahoo.com>
2021-01-26 22:03:47 +0300

    empty projects

---
+++ SamuraiApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SamuraiApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
