862cc974c946971451a9cfbe33b22dbf4df5c814
erythrocyte <32854075+erythrocyte@users.noreply.github.com>
2021-02-05 23:54:36 +0300

    Feature/aspnet (#6)
    * asp net project
    
    * debugging is not working
    
    * weather casr deleted

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,40 +1,24 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+            :base (options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Battle> Battles {get;set;}
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connectionString =
-            "Server=localhost;Database=SamuraiAppData;User=SA;Password=Samurai_01";
-            optionsBuilder
-                .UseLoggerFactory(ConsoleLoggerFactory)
-                .EnableSensitiveDataLogging()
-                .UseSqlServer(connectionString);
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
             modelBuilder.Entity<Horse>().ToTable("Horses");
         }
-
-        public static readonly ILoggerFactory ConsoleLoggerFactory
-            = LoggerFactory.Create(builder =>
-            {
-                builder
-                    .AddFilter((category, level) =>
-                        category == DbLoggerCategory.Database.Command.Name
-                        && level == LogLevel.Information)
-                    .AddConsole();
-            });
     }
 }
