4a040abf289d10fa549df6073df297b14a0c4775
erythrocyte <32854075+erythrocyte@users.noreply.github.com>
2021-02-06 08:38:38 +0300

    Feature/testing (#7)
    * asp net project
    
    * debugging is not working
    
    * weather casr deleted
    
    * testing completed

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -5,6 +5,8 @@ namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public SamuraiContext() { }
+        
         public SamuraiContext(DbContextOptions<SamuraiContext> options)
             :base (options)
         {
@@ -13,7 +15,7 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
-        public DbSet<Battle> Battles {get;set;}
+        public DbSet<Battle> Battles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
